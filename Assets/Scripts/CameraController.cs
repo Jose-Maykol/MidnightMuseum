46d9a29469b6e8cf781828b13721a64607221481
José Maykol <jpaniura@unsa.edu.pe>
2023-10-21 02:38:23 -0500

    first person camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Transform camera;
+    public Transform target; // Objetivo de la cámara
     public Vector2 mouseSensitivity = new Vector2(1, 1); // Sensibilidad del mouse
 
     void Start()
     {
-        camera = transform.Find("Camera"); // Obtiene la cámara
         Cursor.lockState = CursorLockMode.Locked; // Oculta el cursor
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 newPosition = new Vector3(target.position.x, transform.position.y, target.position.z);
+        transform.position = newPosition; // Iguala la posición de la cámara a la del objetivo
+
         float mouseX = Input.GetAxis("Mouse X"); // Obtiene el movimiento del mouse en el eje X
         float mouseY = Input.GetAxis("Mouse Y"); // Obtiene el movimiento del mouse en el eje Y
 
@@ -24,11 +26,18 @@ public class CameraController : MonoBehaviour
             transform.Rotate(Vector3.up * mouseX * mouseSensitivity.x); // Rota la cámara
         }
         if (mouseY != 0) // Si el mouse se movió
-        {
-            float angle = (camera.localEulerAngles.x - mouseY * mouseSensitivity.y + 360) % 360; // Obtiene el ángulo de la cámara
-            if (angle > 180) angle -= 360; // Si el ángulo es mayor a 180, lo convierte a negativo
-            angle = Mathf.Clamp(angle, -80, 80); // Limita el ángulo de la cámara
-            camera.localEulerAngles = Vector3.right * angle; // Rota la cámara
+        {   
+            Vector3 rotation = transform.localEulerAngles; // Obtiene la rotación de la cámara
+            rotation.x = (rotation.x - mouseY * mouseSensitivity.y + 360) % 360; // Obtiene el ángulo de la cámara
+            if (rotation.x > 80 && rotation.x < 180) // Si el ángulo es mayor a 80 y menor a 180
+            {
+                rotation.x = 80; // El ángulo es 80
+            }
+            else if (rotation.x < 280 && rotation.x > 180) // Si el ángulo es menor a 280 y mayor a 180
+            {
+                rotation.x = 280; // El ángulo es 280
+            }
+            transform.localEulerAngles = rotation; // Aplica la rotación a la cámara
         }
     }
 }
